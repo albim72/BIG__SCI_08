4142c9f29e85219f5b7d0a2554aa344ca0e79a33
albim72 <marcin.albiniak72@gmail.com>
2024-08-06 10:26:15 +0200

    Create main.py

---
+++ DZIEN_1/moj_moduly/main.py
@@ -0,0 +1,15 @@
+# import dane
+# import dane as dn
+
+from dane import nrfilii as nf
+from dane import book as bk
+
+print("to jest program modułowy!")
+print("Przydatne skróty:")
+print("CTRL + D -> powielenie linii/bloku tekstu")
+print("CTRL + /  -> komentowanie/odkomentowanie bloku kodu")
+print("___________ lista -> nrfilii _________")
+print(nf)
+
+print("___________ słownik -> book _________")
+print(bk)
