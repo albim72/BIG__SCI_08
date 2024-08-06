9e4059d5a77288e41fb025a6198365cf9f592e82
albim72 <marcin.albiniak72@gmail.com>
2024-08-06 11:00:42 +0200

    Update main.py

--- DZIEN_1/moj_moduly/main.py
+++ DZIEN_1/moj_moduly/main.py
@@ -4,6 +4,8 @@
 from dane import nrfilii as nf
 from dane import book as bk
 
+from moje_funkcje.kolekcje_f import czytaj_liste,czytaj_slownik
+
 print("to jest program modułowy!")
 print("Przydatne skróty:")
 print("CTRL + D -> powielenie linii/bloku tekstu")
@@ -13,3 +15,9 @@ print(nf)
 
 print("___________ słownik -> book _________")
 print(bk)
+
+print("___________ lista -> nrfilii -> funkcja czytaj_liste() _________")
+czytaj_liste(nf)
+
+print("___________ słownik -> book -> funkcja czytaj_slownik() _________")
+czytaj_slownik(bk)
